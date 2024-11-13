1331660783e3cb825dfa5d491d93e62dd3928965
Alex Khilko <alex.khilko@tripledotstudios.com>
2024-11-13 20:27:50 +0100

    Add support for nested json

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ import argparse
 from enum import auto, Enum
 
 
+# typical lifetime: NOT_STARTED -> WAIT_KEY -> COLLECTING_KEY -> END_KEY -> WAIT_VALUE -> COLLECTING_VALUE -> END_VALUE -> FINISHED
 class Status(Enum):
     NOT_STARTED = auto()
     COLLECTING_KEY = auto()
@@ -12,28 +13,25 @@ class Status(Enum):
     WAIT_VALUE = auto()
     FINISHED = auto()
 
-
 to_skip = (" ", "\n")
 
-
 class JsonLoader:
-    def __init__(self):
+    def __init__(self, status = Status.NOT_STARTED):
         self.tokens = []
         self.result = {}
-        self.status = Status.NOT_STARTED
+        self.status = status
         self.result = {}
         self.cur_token = ""
-    
+
     def handle_not_started(self, char: str) -> None:
+        if char in to_skip:
+            return
         if char in {"[", "{"}:
             self.status = Status.WAIT_KEY
-            return
-        if char in to_skip:
             return
         raise Exception(
             f"Invalid json: Invalid character {char} before start. status - not started"
         )
-
 
     def handle_collecting_key(self, char: str) -> None:
         if char == '"':
@@ -57,8 +55,9 @@ class JsonLoader:
         if char == ":":
             self.status = Status.WAIT_VALUE
         else:
-            raise Exception(f"Invalid json: expected semicolon. Found {char}. status - end key")
-
+            raise Exception(
+                f"Invalid json: expected semicolon. Found {char}. status - end key"
+            )
 
     def handle_end_value(self, char: str) -> None:
         if char in to_skip:
@@ -68,8 +67,9 @@ class JsonLoader:
         elif char == "}":
             self.status = Status.FINISHED
         else:
-            raise Exception(f"Invalid json: expected comma. Found {char}. status - end value")
-
+            raise Exception(
+                f"Invalid json: expected comma. Found {char}. status - end value"
+            )
 
     def handle_wait_key(self, char: str) -> None:
         if char == "}" and not self.tokens:
@@ -83,8 +83,9 @@ class JsonLoader:
         elif char == "}" and self.tokens:
             raise Exception(f"Invalid json: waiting for new key. Found {char}")
         else:
-            raise Exception(f"Invalid json: expecting quotes. Found {char}. status - wait key")
-
+            raise Exception(
+                f"Invalid json: expecting quotes. Found {char}. status - wait key"
+            )
 
     def handle_wait_value(self, char: str) -> None:
         if char in to_skip:
@@ -97,14 +98,6 @@ class JsonLoader:
                 f"Invalid json: expecting quotes. Found {char}. status - wait value"
             )
 
-
-    def handle_finished(self, char: str) -> None:
-        if char in to_skip:
-            return
-        raise Exception(
-            f"Invalid json: expecting no characters after close bracket. Found {char}. status - finished"
-        )
-
     def get_handler(self):
         handler_map = {
             Status.NOT_STARTED: self.handle_not_started,
@@ -114,33 +107,43 @@ class JsonLoader:
             Status.END_VALUE: self.handle_end_value,
             Status.WAIT_KEY: self.handle_wait_key,
             Status.WAIT_VALUE: self.handle_wait_value,
-            Status.FINISHED: self.handle_finished,
         }
         return handler_map[self.status]
-    
+
     def load_result(self):
         if self.status is Status.END_VALUE and self.tokens:
             self.result[self.tokens[-2]] = self.tokens[-1]
-    
-    def load(self, file_path: str):
-        with open(file_path) as f:
-            char = f.read(1)
-            while char:
+
+    def load(self, f):
+        char = f.read(1)
+        while char and self.status is not Status.FINISHED:
+            if self.status is Status.WAIT_VALUE and char == "{":
+                result = JsonLoader(status=Status.WAIT_KEY).load(f)
+                self.tokens.append(result)
+                self.status = Status.END_VALUE
+            else:
                 handler = self.get_handler()
                 handler(char)
-                self.load_result()
-                char = f.read(1)
+            self.load_result()
+            char = f.read(1)
         if self.status is Status.NOT_STARTED:
             raise Exception("Invalid json - Empty file.")
         return self.result
 
-# {"key": "value"}
-# {"key": {"key2": "value"}}
 
 def load(file_path: str) -> list[str]:
     reader = JsonLoader()
-    return reader.load(file_path)
-
+    result = None
+    with open(file_path) as f:
+        result = reader.load(f)
+        char = f.read(1)
+        while char:
+            if char not in to_skip:
+                raise Exception(
+                    f"Invalid json: expecting no characters after close bracket. Found {char}. status - finished"
+                )
+            char = f.read(1)
+    return result
 
 # def load(file_path: str) -> list[str]:
 #     tokens = []
--- tests/test_load.py
+++ tests/test_load.py
@@ -2,19 +2,28 @@ from ..parser import load
 import pytest
 
 
-@pytest.mark.parametrize("path,expected", [
+@pytest.mark.parametrize(
+    "path,expected",
+    [
         ("tests/test_files/valid.json", {}),
         ("tests/test_files/valid2.json", {"key": "value"}),
         ("tests/test_files/valid3.json", {"key": "value", "key2": "value"}),
-])
+        (
+            "tests/test_files/valid_nested.json",
+            {"key": "value", "key2": {"key3": "value3", "key4": {"key5": "value5"}}},
+        ),
+    ],
+)
 def test_load(path, expected):
     result = load(path)
     assert result == expected
 
 
-@pytest.mark.parametrize("path", [
+@pytest.mark.parametrize(
+    "path",
+    [
         "tests/test_files/invalid_empty.json",
-    ]
+    ],
 )
 def test_load_empty(path):
     with pytest.raises(Exception) as e:
@@ -22,9 +31,11 @@ def test_load_empty(path):
     assert "Invalid json" in str(e.value)
 
 
-@pytest.mark.parametrize("path", [
+@pytest.mark.parametrize(
+    "path",
+    [
         "tests/test_files/invalid_key.json",
-    ]
+    ],
 )
 def test_fail_invali_key(path):
     with pytest.raises(Exception) as e:
@@ -32,9 +43,11 @@ def test_fail_invali_key(path):
     assert "Invalid json: expecting quotes" in str(e.value)
 
 
-@pytest.mark.parametrize("path", [
+@pytest.mark.parametrize(
+    "path",
+    [
         "tests/test_files/invalid_extra_comma.json",
-    ]
+    ],
 )
 def test_fail_extra_comma(path):
     with pytest.raises(Exception) as e:
@@ -42,9 +55,11 @@ def test_fail_extra_comma(path):
     assert "Invalid json: waiting for new key" in str(e.value)
 
 
-@pytest.mark.parametrize("path", [
+@pytest.mark.parametrize(
+    "path",
+    [
         "tests/test_files/invalid_after_end.json",
-    ]
+    ],
 )
 def test_fail_after_end(path):
     with pytest.raises(Exception) as e:
@@ -52,9 +67,11 @@ def test_fail_after_end(path):
     assert "Invalid json: expecting no characters after close bracket" in str(e.value)
 
 
-@pytest.mark.parametrize("path", [
+@pytest.mark.parametrize(
+    "path",
+    [
         "tests/test_files/invalid_between_tokens.json",
-    ]
+    ],
 )
 def test_fail_between_tokens(path):
     with pytest.raises(Exception) as e:
